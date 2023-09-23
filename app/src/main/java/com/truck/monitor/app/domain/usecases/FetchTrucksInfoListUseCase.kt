cf9c60f8ec797a8a8e18f27af1e3e4114d606f62
Junyd Dev <junyd.dev@gmail.com>
2023-09-23 22:59:45 +0400

    Created use case for fetching trucks info list.

---
+++ app/src/main/java/com/truck/monitor/app/domain/usecases/FetchTrucksInfoListUseCase.kt
@@ -0,0 +1,7 @@
+package com.truck.monitor.app.domain.usecases
+
+import com.truck.monitor.app.data.repository.TrucksInfoRepository
+
+class FetchTrucksInfoListUseCase(private val repository: TrucksInfoRepository) {
+
+}
