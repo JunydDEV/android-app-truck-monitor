8cf220ad00c2dfbfa36083d508a88c6599846b7c
Junyd Dev <junyd.dev@gmail.com>
2023-09-24 01:06:24 +0400

    Added handle function to the ExceptionHandler class to handle some known failure cases.

--- app/src/main/java/com/truck/monitor/app/data/repository/ExceptionHandler.kt
+++ app/src/main/java/com/truck/monitor/app/data/repository/ExceptionHandler.kt
@@ -1,5 +1,42 @@
 package com.truck.monitor.app.data.repository
 
+import com.google.gson.JsonParseException
+import com.truck.monitor.app.data.model.DataRequestError
+import org.json.JSONException
+import java.io.IOException
 import javax.inject.Inject
 
-class ExceptionHandler @Inject constructor()
+class ExceptionHandler @Inject constructor() {
+
+    fun handle(it: Throwable): DataRequestError {
+        return when (it) {
+            is IOException -> {
+                DataRequestError(
+                    message = "Network Error",
+                    description = "Make sure your device is connected to a working internet."
+                )
+            }
+
+            is JSONException -> {
+                DataRequestError(
+                    message = "Invalid Response Error",
+                    description = "Something went wrong on server side, please try again."
+                )
+            }
+
+            is JsonParseException -> {
+                DataRequestError(
+                    message = "Invalid Response Error",
+                    description = "Something went wrong on server side, please try again."
+                )
+            }
+
+            else -> {
+                DataRequestError(
+                    message = "Unknown Error",
+                    description = "Request failed due to unknown reasons, please try later."
+                )
+            }
+        }
+    }
+}
