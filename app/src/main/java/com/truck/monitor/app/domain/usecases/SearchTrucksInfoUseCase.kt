8d429c2bafbc9c79fed9652c0229037b3b55c65e
Junyd Dev <junyd.dev@gmail.com>
2023-09-23 22:59:55 +0400

    Created use case for searching trucks info list.

---
+++ app/src/main/java/com/truck/monitor/app/domain/usecases/SearchTrucksInfoUseCase.kt
@@ -0,0 +1,7 @@
+package com.truck.monitor.app.domain.usecases
+
+import com.truck.monitor.app.data.repository.TrucksInfoRepository
+
+class SearchTrucksInfoUseCase(private val repository: TrucksInfoRepository) {
+
+}
