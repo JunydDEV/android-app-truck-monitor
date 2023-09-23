c9f889c50087bc89f6f9e2b19753e61e54143587
Junyd Dev <junyd.dev@gmail.com>
2023-09-23 23:00:06 +0400

    Created use case for sorting trucks info list.

---
+++ app/src/main/java/com/truck/monitor/app/domain/usecases/SortTrucksInfoListingUseCase.kt
@@ -0,0 +1,7 @@
+package com.truck.monitor.app.domain.usecases
+
+import com.truck.monitor.app.data.repository.TrucksInfoRepository
+
+class SortTrucksInfoListingUseCase(private val repository: TrucksInfoRepository) {
+
+}
