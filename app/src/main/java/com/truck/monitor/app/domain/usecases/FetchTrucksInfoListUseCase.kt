c8510158adbb2e23e9f6ec19bf785e11e3737ec6
Junyd Dev <junyd.dev@gmail.com>
2023-09-24 01:10:24 +0400

    Added fetch trucks info list call to the FetchTrucksInfoListUseCase and mapped the response on UiState.

--- app/src/main/java/com/truck/monitor/app/domain/usecases/FetchTrucksInfoListUseCase.kt
+++ app/src/main/java/com/truck/monitor/app/domain/usecases/FetchTrucksInfoListUseCase.kt
@@ -1,10 +1,40 @@
 package com.truck.monitor.app.domain.usecases
 
+import com.truck.monitor.app.data.model.DataState
+import com.truck.monitor.app.data.model.TruckInfo
 import com.truck.monitor.app.data.repository.TrucksInfoRepository
+import com.truck.monitor.app.ui.state.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class FetchTrucksInfoListUseCase @Inject constructor(
     private val repository: TrucksInfoRepository
 ) {
+    private val _trucksInfoListStateFlow = MutableStateFlow<UiState>(UiState.OnStart)
+    val trucksInfoListStateFlow: StateFlow<UiState> = _trucksInfoListStateFlow
 
+    suspend operator fun invoke() {
+        repository.fetchTrucksInfoList()
+            .onStart {
+                _trucksInfoListStateFlow.value = UiState.OnLoading
+            }
+            .collect {
+                when (it) {
+                    is DataState.OnError -> {
+                        val errorObject = it.response
+                        _trucksInfoListStateFlow.value = UiState.OnFailure(
+                            error = errorObject.message,
+                            errorDescription = errorObject.description
+                        )
+                    }
+
+                    is DataState.OnSuccess<*> -> {
+                        val list = it.response as List<TruckInfo>
+                        _trucksInfoListStateFlow.value = UiState.OnSuccess(list)
+                    }
+                }
+            }
+    }
 }
--- app/src/main/java/com/truck/monitor/app/ui/listingscreen/ListingScreenModel.kt
+++ app/src/main/java/com/truck/monitor/app/ui/listingscreen/ListingScreenModel.kt
@@ -1,10 +1,12 @@
 package com.truck.monitor.app.ui.listingscreen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.truck.monitor.app.domain.usecases.FetchTrucksInfoListUseCase
 import com.truck.monitor.app.domain.usecases.SearchTrucksInfoUseCase
 import com.truck.monitor.app.domain.usecases.SortTrucksInfoListingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +14,13 @@ class ListingScreenModel @Inject constructor(
     private val fetchTrucksInfoListUseCase: FetchTrucksInfoListUseCase,
     private val searchTrucksInfoUseCase: SearchTrucksInfoUseCase,
     private val sortTrucksInfoListingUseCase: SortTrucksInfoListingUseCase
-): ViewModel() {}
+) : ViewModel() {
+
+    val trucksInfoStateFlow = fetchTrucksInfoListUseCase.trucksInfoListStateFlow
+
+    fun fetchTrucksInfoList() {
+        viewModelScope.launch {
+            fetchTrucksInfoListUseCase.invoke()
+        }
+    }
+}
