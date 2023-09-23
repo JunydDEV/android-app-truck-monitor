034554a1d828b92408157b328638331851dd5e0c
Junyd Dev <junyd.dev@gmail.com>
2023-09-23 23:50:48 +0400

    Fixed some fields naming of truck info model.

--- app/src/main/java/com/truck/monitor/app/data/model/TruckInfo.kt
+++ app/src/main/java/com/truck/monitor/app/data/model/TruckInfo.kt
@@ -1,11 +1,11 @@
 package com.truck.monitor.app.data.model
 
 data class TruckInfo(
-    val plotNo: Int,
+    val plateNo: Int,
     val driverName: String,
-    val profileImage: String,
+    val imageURL: String,
     val lat: Double,
     val long: Double,
     val location: String,
-    val lastUpdate: Int
+    val lastUpdated: String
 )
