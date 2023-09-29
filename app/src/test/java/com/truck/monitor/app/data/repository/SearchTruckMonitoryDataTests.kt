4940690d3eb1fd4bca0f8ef7154b48e804b45d83
Junyd Dev <junyd.dev@gmail.com>
2023-09-29 19:57:31 +0400

    Added unit tests for search data in local database.

---
+++ app/src/test/java/com/truck/monitor/app/data/repository/SearchTruckMonitoryDataTests.kt
@@ -0,0 +1,111 @@
+package com.truck.monitor.app.data.repository
+
+import app.cash.turbine.test
+import com.truck.monitor.app.data.SearchResultNotFoundException
+import com.truck.monitor.app.data.datasource.LocalDataSource
+import com.truck.monitor.app.data.datasource.RemoteDataSource
+import com.truck.monitor.app.data.model.DataFailureResponse
+import com.truck.monitor.app.data.model.DataState
+import com.truck.monitor.app.data.model.TruckInfoMapper
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class SearchTruckMonitoryDataTests {
+    @Mock
+    lateinit var remoteDataSource: RemoteDataSource
+
+    @Mock
+    lateinit var localDataSource: LocalDataSource
+
+    @Mock
+    lateinit var exceptionHandler: ExceptionHandler
+
+    private lateinit var truckInfoMapper: TruckInfoMapper
+    private lateinit var sut: TrucksInfoRepository
+
+    @Before
+    fun setup() {
+        truckInfoMapper = TruckInfoMapper(Dispatchers.IO)
+
+        sut = TrucksInfoRepositoryImpl(
+            remoteDatasource = remoteDataSource,
+            localDatasource = localDataSource,
+            truckInfoMapper = truckInfoMapper,
+            exceptionHandler = exceptionHandler,
+            dispatcher = Dispatchers.IO
+        )
+    }
+
+    @Test
+    fun `searchTruckInfo when local datasource finds the record then returns non-empty results`() =
+        runTest {
+            val query = "Ali"
+            val truckInfoDataDtoList = listOf(
+                truckInfoMapper.toTruckInfoListItemDto(validTruckInfoItem1),
+            )
+            whenever(localDataSource.searchTruckInfo(query)).thenReturn(truckInfoDataDtoList)
+
+            sut.searchTruckInfo(query).test {
+                val dataState = awaitItem()
+                assertTrue(dataState is DataState.OnSuccess<*>)
+
+                val successState = dataState as DataState.OnSuccess<*>
+                assertNotNull(successState.response)
+                assertNotNull(successState.response.data)
+
+                val truckInfoListItemDto = successState.response.data as List<*>
+                assertNotNull(truckInfoListItemDto)
+                assertTrue(truckInfoListItemDto.isNotEmpty())
+                assertTrue(truckInfoListItemDto.size == 1)
+
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+
+    @Test
+    fun `searchTruckInfo when local datasource doesn't find the record returns failure error object`() =
+        runTest {
+            val query = "Lorem Ipsum"
+            val searchNotFoundTitle = "Search Result Not Found"
+            val searchNotFoundDescription =
+                "Couldn't found any results against your query, try different query"
+            val searchNotFoundException = SearchResultNotFoundException(
+                title = searchNotFoundTitle,
+                description = searchNotFoundDescription
+            )
+            val error = DataFailureResponse(
+                searchNotFoundException.title,
+                searchNotFoundException.description
+            )
+            whenever(localDataSource.searchTruckInfo(query)).thenAnswer { throw searchNotFoundException }
+            whenever(exceptionHandler.handle(searchNotFoundException)).thenReturn(error)
+
+            sut.searchTruckInfo(query).test {
+                val dataState = awaitItem()
+                assertTrue(dataState is DataState.OnError)
+
+                val errorState = dataState as DataState.OnError
+                val errorResponse = errorState.response
+                assertNotNull(errorResponse)
+                assertNotNull(errorResponse.message)
+                assertNotNull(errorResponse.description)
+
+                assertEquals(searchNotFoundTitle, errorResponse.message)
+                assertEquals(searchNotFoundDescription, errorResponse.description)
+
+                verify(exceptionHandler).handle(searchNotFoundException)
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+}
